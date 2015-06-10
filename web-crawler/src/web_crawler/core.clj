980a3ca45d2aa41ed5d941823c326e41b43d7ca3
Cvarog <uchihamilord@gmail.com>
2015-06-10 19:05:45 +0300

    fixed

--- web-crawler/src/web_crawler/core.clj
+++ web-crawler/src/web_crawler/core.clj
@@ -5,7 +5,6 @@
 (defn -main []
   (with-open [input (FileInputStream. "site.html")]
     (let [doc (Jsoup/parse input "UTF-8" "http://en.wikipedia.org/wiki/Main_Page")
-    ;(let [doc (Jsoup/get (Jsoup/connect "http://noob-club.com"))
           links (.select doc "a[href]")]
       (doseq [link links]
         (println (.attr link "abs:href"))))))
